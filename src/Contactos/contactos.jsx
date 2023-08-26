3d37a86da8400997394c374111344537206e4b02
Helder Barbedo <8220234@estg.ipp.pt>
2023-08-26 15:56:03 +0100

    contactos

--- src/Contactos/contactos.jsx
+++ src/Contactos/contactos.jsx
@@ -35,10 +35,45 @@ function contactos() {
                 Quem eu sou?
               </div>
             </div>
-            <div className="grid-1-1 margin-top-20">
-              <div className="label-size-16 labelColorGray margin-top-15">
-                Estou interessado em oportunidades freelance. Se tiver algum
-                pedido ou questão, não hesite em contactar me
+            <div className="grid-1-1 grid-flex margin-top-20">
+              <div className="grid-1-1 label-size-16 labelColorGray margin-top-15">
+                Estou interessado em oportunidades freelance. <br />
+                No entanto, se você tiver outro pedido ou pergunta, <br />
+                não hesite em contactar-me
+              </div>
+              <div className="grid-1-1">
+                <motion.div
+                  className="width230 height120 height-reset borderGray1  border-radius-5 padding10"
+                  ref={Contactosref}
+                  variants={{
+                    hidden: { opacity: 0, x: 75 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                  initial="hidden"
+                  animate={mainControlsContactos}
+                  transition={{ delay: 0.2, duration: 0.5 }}>
+                  <div className="grid-1-1 labelColorWhite label-size-16 label-weight-semibold">
+                    Contacte me aqui!!!
+                  </div>
+                  <div className="grid-1-1 grid-flex margin-top-10">
+                    <div className="linkedin icon-size-20 icon-align-center width15 height15 margin-top-5"></div>
+                    <div className="grid-flex-grow height30 labelColorGray label-size-14 margin-left-5">
+                      Hélder Barbedo
+                    </div>
+                  </div>
+                  <div className="grid-1-1 grid-flex">
+                    <div className="wpp icon-size-20 icon-align-center width15 height15 margin-top-5"></div>
+                    <div className="grid-flex-grow height30 labelColorGray label-size-14 margin-left-5">
+                      +351 912911029
+                    </div>
+                  </div>
+                  <div className="grid-1-1 grid-flex">
+                    <div className="email icon-size-20 icon-align-center width15 height15 margin-top-5"></div>
+                    <div className="grid-flex-grow height30 labelColorGray label-size-14 margin-left-5">
+                      helderbarbedowork@gmail.com
+                    </div>
+                  </div>
+                </motion.div>
               </div>
             </div>
           </motion.div>
